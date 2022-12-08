dc8b7e39a19dd5805f2f254c6e300d8a5db2a8cd
Seth Wooten <slwooten14@gmail.com>
2022-12-08 16:28:38 -0500

    fixed margin on all sections

--- src/components/Quote/index.jsx
+++ src/components/Quote/index.jsx
@@ -8,7 +8,7 @@ const Quote = () => {
   // intersection observer
   const [shown, setShown] = useState(false);
   const { ref, inView, entry } = useInView({
-    threshold: 0,
+    rootMargin: '-25%',
     onChange: (inView) => {
       if (inView) {
         setShown(true);
@@ -17,9 +17,9 @@ const Quote = () => {
   });
 
   return (
-    <div ref={ref} className='quote-container'>
-      <h2 className={shown ? 'grow' : 'hidden'}>Here is a quote about programmin', it's lots of fun.</h2>
-      <p className={shown ? 'grow' : 'hidden'}>- Some programmer</p>
+    <div ref={ref} className={shown ? 'quote-container grow' : 'hidden'}>
+      <h2>Here is a quote about programmin', it's lots of fun.</h2>
+      <p>- Some programmer</p>
     </div>
   );
 };
