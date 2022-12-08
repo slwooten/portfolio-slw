af6b26f41bee4b39ef9337171fda565096c8c0d9
Seth Wooten <slwooten14@gmail.com>
2022-12-08 14:09:39 -0500

    added animation to quote section

--- src/components/Quote/index.jsx
+++ src/components/Quote/index.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 import './quote.css';
 
 const Quote = () => {
+
+  // intersection observer - JOB
+  const [shown, setShown] = useState(false);
+  const { ref, inView, entry } = useInView({
+    threshold: 0,
+    onChange: (inView) => {
+      if (inView) {
+        setShown(true);
+      }
+    }
+  });
+
   return (
-    <div className='quote-container'>
-      <h2>Here is a quote about programmin', it's lots of fun.</h2>
-      <p>- Some programmer</p>
+    <div ref={ref} className='quote-container'>
+      <h2 className={shown ? 'grow' : 'hidden'}>Here is a quote about programmin', it's lots of fun.</h2>
+      <p className={shown ? 'grow' : 'hidden'}>- Some programmer</p>
     </div>
   );
 };
